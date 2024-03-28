ff9119b9600e3ac41f768ca7e381ed777a1baea0
Phillip Nguyen <philn0415@gmail.com>
2024-03-27 17:29:23 -0700

    created validation handler

---
+++ backend/utils/validation.js
@@ -0,0 +1,27 @@
+// backend/utils/validation.js
+const { validationResult } = require('express-validator');
+
+// Validation handler
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = {};
+        validationErrors
+            .array()
+            .forEach(error => errors[error.path] = error.msg);
+
+        const err = Error('Bad Request.');
+        err.errors = errors;
+        err.status = 400;
+        err.title = 'Bad request.';
+        next();
+    }
+    next();
+};
+
+module.exports = {
+ handleValidationErrors
+};
