9e4cf6068a294ae65cc51ff5cb617395e693d970
Phillip Nguyen <philn0415@gmail.com>
2024-06-02 20:22:41 -0700

    signup form

---
+++ frontend/src/components/SignupFormPage/SignupFormPage.jsx
@@ -0,0 +1,122 @@
+import { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import './SignupForm.css';
+
+function SignupFormPage() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [errors, setErrors] = useState({});
+
+    if (sessionUser) return <Navigate to="/" replace={true} />;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+          setErrors({});
+          return dispatch(
+            sessionActions.signup({
+              email,
+              username,
+              firstName,
+              lastName,
+              password
+            })
+          ).catch(async (res) => {
+            const data = await res.json();
+            if (data?.errors) {
+              setErrors(data.errors);
+            }
+          });
+        }
+        return setErrors({
+          confirmPassword: "Confirm Password field must be the same as the Password field"
+        });
+      };
+
+    return (
+        <>
+        <h1>Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                >
+                </input>
+            </label>
+            {errors.username && <p style={{color: 'red'}}>{errors.username}</p>}
+            <label>
+                Email
+                <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                >
+                </input>
+            </label>
+            {errors.email && <p style={{color: 'red'}}>{errors.email}</p>}
+            <label>
+                Password
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                >
+                </input>
+            </label>
+            {errors.password && <p style={{color: 'red'}}>{errors.password}</p>}
+            <label>
+                Confirm Password
+                <input
+                    type="text"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                >
+                </input>
+            </label>
+            {errors.confirmPassword && <p style={{color: 'red'}}>{errors.confirmPassword}</p>}
+            <label>
+                First Name
+                <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                >
+                </input>
+            </label>
+            {errors.firstName && <p style={{color: 'red'}}>{errors.firstName}</p>}
+            <label>
+                Last Name
+                <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                >
+                </input>
+            </label>
+            {errors.lastName && <p style={{color: 'red'}}>{errors.lastName}</p>}
+            <button type="submit">Log In</button>
+        </form>
+        </>
+    )
+
+}
+
+
+export default SignupFormPage;
