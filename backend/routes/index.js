ef0e0edf1c9f95600afe7e2505e2d22563db4f31
Phillip Nguyen <philn0415@gmail.com>
2024-06-03 01:42:33 -0700

    deploy changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,3 @@
-// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
@@ -29,12 +28,16 @@ if (process.env.NODE_ENV === 'production') {
   });
 };
 
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
   });
-};
+}
 
 // Add a XSRF-TOKEN cookie
 router.get('/api/csrf/restore', async(req, res) => {
