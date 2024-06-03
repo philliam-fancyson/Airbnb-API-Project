827672049a6845608894265a668abcf1440bcd88
Phillip Nguyen <philn0415@gmail.com>
2024-06-02 19:39:55 -0700

    created session actions and reducer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
+import * as sessionActions from './store/session';
+
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
@@ -13,6 +15,7 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 if (process.env.NODE_ENV !== 'production') {
