db2999d7e71d40ad129417fc3888603395bdd413
Phillip Nguyen <philn0415@gmail.com>
2024-06-03 15:37:51 -0700

    landing store works

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux"
+import { getAllSpots } from "../../store/spot";
+import { useDispatch } from "react-redux";
 
 
 function LandingPage() {
+    const dispatch = useDispatch();
+    const allSpots = useSelector(state => state.spot.spots);
+
+    useEffect(() => {
+        dispatch(getAllSpots())
+    },[dispatch])
+
     return (
         <div>
-
+            <h1>TEST</h1>
+            {allSpots.map((spot) =>
+                <div key={spot.id}>
+                    <h2>{spot.city}, {spot.state}</h2>
+                    <p>{spot.avgRating ? spot.avgRating : "Star Placeholder"}</p>
+                    <p></p>
+                </div>
+            )}
         </div>
     )
 }
+
+export default LandingPage
--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -2,35 +2,29 @@ import { csrfFetch } from './csrf';
 
 const LOAD_ALL_SPOTS = "spot/loadAllSpots";
 
-const load = (list) => {
+const load = (spots) => {
     return {
         type: LOAD_ALL_SPOTS,
-        list
-    }
-}
-
-export const getAllSpots = () => async dispatch => {
-    const response = await fetch(`/api/spots`);
-
-    if (response.ok) {
-        const list = await response.json();
-        dispatch(load(list));
+        spots
     }
 };
 
-const initialState = { spot:null }
+export const getAllSpots = () => async dispatch => {
+    const response = await csrfFetch(`/api/spots`);
+
+    if (response.ok) {
+        const list = await response.json();
+        dispatch(load(list.Spots));
+        return list
+    }
+};
+
+const initialState = { spots: [] }
 
 const spotReducer = (state = initialState, action) => {
     switch(action.type) {
         case LOAD_ALL_SPOTS:
-            const allSpots = {};
-            action.list.forEach(spot => {
-                allSpots[spot.id] = spot;
-            });
-            return {
-                ...state,
-                list: allSpots
-            }
+            return {  ...state, spots: action.spots }
         default:
             return state;
     }
