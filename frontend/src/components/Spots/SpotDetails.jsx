da11353f19714aa6b4bb4fad2fd06a9c28520358
Phillip Nguyen <philn0415@gmail.com>
2024-06-07 10:16:31 -0700

    css and file tweaks

--- frontend/src/components/Spots/SpotDetails.jsx
+++ frontend/src/components/Spots/SpotDetails.jsx
@@ -74,21 +74,25 @@ function SpotDetails () {
     }
 
     return (
-        <div>
+        <div id="spot-details">
             <h2>{spot.name}</h2>
             <h3>{spot.city}, {spot.state}, {spot.country}</h3>
-            <div className="spot-image-gallery">
+            <div id="spot-image-gallery">
                 <img src={spot.previewImage ? "https://picsum.photos/250/300" : "https://picsum.photos/250/300"} />
             </div>
-            <h3>Hosted by {spotOwner?.firstName} {spotOwner?.lastName}</h3>
-            <p>{spot.description}. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div className="spot-callout-box">
-                <h3>${spot.price}</h3>
-                {/* <LuDot /> {spot.numReviews} {spot.numReviews === 1 ? "review" : "reviews"} */}
-                <p>{Number(avgStars) ? <><FaStar /> {avgStars} </>: <><FaStar />{"New"}</>} {numReviews !== 0 ? <><LuDot /> {(numReviews === 1 ? numReviews + " review" : numReviews + " reviews")}</> : null} </p>
-                <button onClick={handleClick}>Reserve</button>
+            <div id="spot-description-box">
+                <div id ="spot-description">
+                    <h3>Hosted by {spotOwner?.firstName} {spotOwner?.lastName}</h3>
+                    <p>{spot.description}. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div id="spot-callout-box">
+                    <h3>${spot.price}</h3>
+                    {/* <LuDot /> {spot.numReviews} {spot.numReviews === 1 ? "review" : "reviews"} */}
+                    <p>{Number(avgStars) ? <><FaStar /> {avgStars} </>: <><FaStar />{"New"}</>} {numReviews !== 0 ? <><LuDot /> {(numReviews === 1 ? numReviews + " review" : numReviews + " reviews")}</> : null} </p>
+                    <button onClick={handleClick}>Reserve</button>
+                </div>
             </div>
-            <div className="spot-reviews">
+            <div id="spot-reviews">
                 <h3>{Number(avgStars) ? <><FaStar /> {avgStars} </>: <><FaStar />{"New"}</>} {numReviews !== 0 ? <><LuDot /> {(numReviews === 1 ? numReviews + " review" : numReviews + " reviews")}</> : null}</h3>
                 {sessionUser && spotOwner?.id !== sessionUserId && nonReviewer && <OpenModalButton
                                                         buttonText="Post Your Review"
@@ -96,7 +100,7 @@ function SpotDetails () {
                                                         modalComponent={<CreateReviewModal spotId={spot.id} sessionUser={sessionUser}/>}
                                                         />}
                 {reviews ? reviews.map(review => (
-                    <div className="review-box" key={review.id}>
+                    <div id="review-box" key={review.id}>
                         <h3>{review.User.firstName}</h3>
                         <p>{new Date(review.createdAt).toLocaleDateString("en-us", options)}</p>
                         <p>{review.review} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
