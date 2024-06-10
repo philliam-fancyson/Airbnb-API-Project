b877c9ba21818de8954b306cf06ff3f2c801e1e4
Phillip Nguyen <philn0415@gmail.com>
2024-06-09 23:55:18 -0700

    deploy ready

--- frontend/src/components/Reviews/DeleteReviewModal.jsx
+++ frontend/src/components/Reviews/DeleteReviewModal.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
 import { removeReview } from '../../store/review';
 import { getAllReviews } from "../../store/review";
+import './DeleteReviewModal.css';
 
 function DeleteReviewModal({reviewId, spotId}) {
     const dispatch = useDispatch();
@@ -14,12 +15,12 @@ function DeleteReviewModal({reviewId, spotId}) {
     };
 
     return (
-        <>
+        <div id="delete-review">
             <h1>Confirm Delete</h1>
             <p>Are you sure you want to delete this review?</p>
             <button onClick={onClick}>Yes (Delete Review)</button>
             <button onClick={closeModal}>No (Keep Review)</button>
-        </>
+        </ div>
     )
 }
 
--- frontend/src/components/Spots/DeleteSpotModal.jsx
+++ frontend/src/components/Spots/DeleteSpotModal.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
 import { removeASpot } from '../../store/spot';
+import './DeleteSpotModal.css'
 
 function DeleteSpotModal({spotId}) {
     const dispatch = useDispatch();
@@ -12,12 +13,12 @@ function DeleteSpotModal({spotId}) {
     };
 
     return (
-        <>
+        <div id="delete-spot">
             <h1>Confirm Delete</h1>
             <p>Are you sure you want to remove this spot from the listings?</p>
             <button onClick={onClick}>Yes (Delete Spot)</button>
             <button onClick={closeModal}>No (Keep Spot)</button>
-        </>
+        </ div>
     )
 }
 
